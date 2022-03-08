c461fa02bf20ec12ec30da4da0bf11cff94974a6
GabriCabrera13 <gabricabrera13@gmail.com>
2022-03-08 10:06:32 -0600

    vista

--- Corte 2/algoritmo.py
+++ Corte 2/algoritmo.py
@@ -1,1 +1,20 @@
-print("Inicio")
+import sys
+from PyQt5 import uic
+from PyQt5.QtWidgets import QMainWindow, QApplication
+import random
+
+class index(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("vistaER_Generica2.ui", self)
+        
+        self.botonIngresar.clicked.connect(self.ingresarDatos)
+    
+    def ingresarDatos(self, ):
+        print('a')
+        
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    GUI = index()
+    GUI.show()
+    sys.exit(app.exec_())
